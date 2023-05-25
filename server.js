3bac78de87a436d618c1cbad524ab0de18d7279d
Morris Schacht <morrisschacht@Morris-MacBook-Pro.local>
2023-05-25 20:55:31 +0200

    1 player is moving in syn

--- server.js
+++ server.js
@@ -4,47 +4,45 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const PORT = 4000;
-
+const PORT = 3000;
 let cubes = {};
 
-//send to client
+// Statische Dateien vom "public" Ordner bereitstellen
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client.html');
 });
 
+
 // Senden Sie die Liste der Cubes an den Client
-io.emit("init", cubes);
+io.on('connection', function(socket) {
+  console.log('user ' + socket.id + ' connected');
+  
+  cubes[socket.id] = {
+    x: Math.floor(Math.random() * 9),
+    y: Math.floor(Math.random() * 11)
+  };
 
+  // Sende die Liste der Cubes an den neu verbundenen Client
+  socket.emit("init", cubes);
 
-//check connection
-io.on('connection', (socket) => {
-  //console.log('a user connected');
-});
-
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-
-
-io.on("connection", function(socket) {
-  console.log("Ein neuer Client hat eine Verbindung hergestellt!");
-
-  // Empfangen Sie den aktuellen Cube-Status vom Client
+  // Aktualisiere den Cube-Status basierend auf den Eingaben des Clients
   socket.on("update-cube", function(position) {
-    // Senden Sie den aktualisierten Cube-Status an alle verbundenen Clients
+    // Füge die Socket-ID zur Position hinzu
+    position.socketID = socket.id;
+    
     io.emit("update-cube", position);
   });
-});
 
+  // Client-Verbindung trennen
+  socket.on('disconnect', () => {
+    console.log('user ' + socket.id + ' disconnected');
+    delete cubes[socket.id];
+    io.emit("update-cube", { socketID: socket.id, remove: true });
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}`);
 });
-
-
-
