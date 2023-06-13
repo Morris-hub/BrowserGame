47145069bff51a99d3c6e664ba1d6263cfdbfe89
morris-hub <sebastiansulzbach@web.de>
2023-06-13 15:59:18 +0200

    added enemy class

--- public/enemy.js
+++ public/enemy.js
@@ -9,9 +9,17 @@ class Enemy  {
 
 
     walk() {
+        const x = Math.random() * 100;
+        const y = Math.random() * 100;
         console.log('walk');
         currentPos = this.x;
     }
+
+     updateCubePosition(position) {
+        cube.style.left = position.x + "px";
+        cube.style.top = position.y + "px";
+      }
+
 
 }
 
